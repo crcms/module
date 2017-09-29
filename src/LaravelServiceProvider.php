8cac9777f3831f49803da03672c362beb79dac08
crcms <crcms@crcms.cn>
2017-09-29 17:19:39 +0800

    init

---
+++ src/LaravelServiceProvider.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace CrCms\Module;
+
+use Illuminate\Support\ServiceProvider;
+
+/**
+ * Class LaravelServiceProvider
+ *
+ * @package CrCms\ElasticSearch
+ * @author simon
+ */
+class LaravelServiceProvider extends ServiceProvider
+{
+    /**
+     * @var string
+     */
+    protected $packagePath = __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR;
+
+    /**
+     * @return void
+     */
+    public function boot()
+    {
+        $this->publishes([
+            $this->packagePath . 'config' => config_path(),
+        ]);
+    }
+
+    /**
+     * @return void
+     */
+    public function register()
+    {
+        //merge config
+        $this->mergeConfig();
+    }
+
+    /**
+     * @return void
+     */
+    protected function  mergeConfig()
+    {
+        $configFile = $this->packagePath . 'config/module.php';
+        if (file_exists($configFile)) {
+            $this->mergeConfigFrom($configFile, 'module');
+        }
+    }
+}
