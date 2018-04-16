03559d57888fab661cc9329d55bfd81de0f52fc2
crcms <crcms@crcms.cn>
2018-04-16 11:02:49 +0000

    route整理

--- routes/api.php
+++ routes/api.php
@@ -6,7 +6,12 @@ Route::prefix('api/v1')->middleware(['api'])->group(function () {
     Route::prefix('manage')->middleware([])->namespace('CrCms\Module\Http\Controllers\Api\Manage')->group(function () {
         Route::get('modules/attributes', 'AttributeController@index')->name('module.attributes.index');
         Route::get('modules/attributes/{id}', 'AttributeController@show')->where('id', '[A-Za-z_]+')->name('module.attributes.show');
-        Route::resource('modules', 'ModuleController');
+        Route::apiResource('modules', 'ModuleController')->names([
+            'index' => 'module.modules.index',
+            'store' => 'module.modules.store',
+            'update' => 'module.modules.update',
+            'destroy' => 'module.modules.destroy',
+        ])->except(['show']);
     });
 });
 
