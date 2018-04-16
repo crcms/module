95878cc9a8e95ff568c146bae71365b6c8d85ffc
crcms <crcms@crcms.cn>
2018-04-16 14:09:13 +0800

    status

--- src/Http/Resources/ModuleResource.php
+++ src/Http/Resources/ModuleResource.php
@@ -3,6 +3,7 @@
 namespace CrCms\Module\Http\Resources;
 
 use CrCms\Foundation\App\Http\Resources\Resource;
+use CrCms\Module\Attributes\ModuleAttribute;
 
 class ModuleResource extends Resource
 {
@@ -16,6 +17,7 @@ class ModuleResource extends Resource
             'id' => $this->id,
             'name' => $this->name,
             'sign' => $this->sign,
+            'status' => ModuleAttribute::getStaticAttributes(ModuleAttribute::KEY_STATUS . '.' . strval($this->status)),
             'namespace' => $this->namespace,
             'created_at' => $this->created_at->toDateTimeString(),
             'updated_at' => $this->updated_at->toDateTimeString(),
